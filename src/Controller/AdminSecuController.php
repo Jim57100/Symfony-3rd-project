7fbda7fbe2d3f4ef8d3cfabc35b70db43911c80b
Jean-Michael SALICE <jmsalice@gmail.com>
2021-07-15 18:21:21 +0200

    Adding roles to users

--- src/Controller/AdminSecuController.php
+++ src/Controller/AdminSecuController.php
@@ -8,25 +8,46 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminSecuController extends AbstractController
 {
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function index(Request $request, ObjectManager $om) //On récupère les données du formulaire via l'injection de dépendance
+    public function index(Request $request, ObjectManager $om, UserPasswordEncoderInterface $encoder) //On récupère les données du formulaire via l'injection de dépendance
     {
         $utilisateur = new Utilisateur;
         $form = $this->createForm(InscriptionType::class, $utilisateur);
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $passwordCrypt = $encoder->encodePassword($utilisateur, $utilisateur->getPassword()); //On encode le mdp
+            $utilisateur->setPassword($passwordCrypt); 
             $om->persist($utilisateur);
             $om->flush();
+            return $this->redirectToRoute("aliments");
         }
 
         return $this->render('admin_secu/inscription.html.twig',[
             "form" => $form->createView()
         ]);
     }
+    /**
+     * @Route("/login", name="connexion")
+     */
+    public function login(AuthenticationUtils $util){
+        return $this->render("admin_secu/login.html.twig", [
+            "lastUserName" => $util->getLastUsername(),
+            "error" => $util->getLastAuthenticationError()
+        ]);
+    }
+    /**
+     * @Route("/logout", name="deconnexion")
+     */
+    public function logout(){
+       
+    }
+
 }
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -2,12 +2,20 @@
 
 namespace App\Entity;
 
+use App\Entity\Utilisateur;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ * @UniqueEntity(
+ * fields={"username"},
+ * message="Le login choisi existe déjà"
+ * )
  */
-class Utilisateur
+class Utilisateur implements UserInterface 
 {
     /**
      * @ORM\Id()
@@ -18,17 +26,26 @@ class Utilisateur
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=5, max=15, minMessage="Il faut plus de 5 caractères", maxMessage="Il faut moins de 15 caractères")
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=5, max=15, minMessage="Il faut plus de 5 caractères", maxMessage="Il faut moins de 15 caractères")
      */
     private $password;
 
-
-
+    /**
+     * @Assert\Length(min=5, max=15, minMessage="Il faut plus de 5 caractères", maxMessage="Il faut moins de 15 caractères")
+     * @Assert\EqualTo(propertyPath="password", message="Les mots de passe ne sont pas équivalents")
+     */
     private $verificationPassword;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
 
     public function getId(): ?int
     {
@@ -67,6 +84,31 @@ class Utilisateur
     public function setVerificationPassword(string $verificationPassword): self
     {
         $this->verificationPassword = $verificationPassword;
+
+        return $this;
+    }
+
+    //Ajout des fonctions de sécurité
+    public function eraseCredentials() {
+
+    }
+
+    public function getSalt() {
+
+    }
+
+    public function getRoles() {
+        return [$this->roles];
+    }
+
+    public function setRoles(?string $roles): self
+    {
+        //De base l'utilisateur qui s'inscrit aura le role ROLE_USER
+        if($roles === null) {
+            $this->roles = "ROLE_USER";
+        } else {
+            $this->roles = $roles;
+        }
 
         return $this;
     }
